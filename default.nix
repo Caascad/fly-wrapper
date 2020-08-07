1ec66bddfd82e87387ab22c1ca562868ab45bce8
lightcode <lightcode@users.noreply.github.com>
2020-08-07 14:38:31 +0200

    Add yq as a dependency

--- default.nix
+++ default.nix
@@ -9,10 +9,13 @@ stdenv.mkDerivation {
   pname = "fly-wrapper";
   version = "0.0.1";
 
+  nativeBuildInputs = [ makeWrapper ];
+
   unpackPhase = ":";
   installPhase = ''
     mkdir -p $out/bin
     cp ${./fly-wrapper} $out/bin/fly-wrapper
     chmod +x $out/bin/fly-wrapper
+    wrapProgram $out/bin/fly-wrapper --prefix PATH : ${lib.makeBinPath [ yq ]}
   '';
 }
